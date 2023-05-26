85818ca400cd68166dcce8707a3c80b514654b62
My1758 <134392467+My1758@users.noreply.github.com>
2023-05-26 15:38:21 +0300

    Update PeopleProcessor.java

--- src/main/java/efs/task/functional/PeopleProcessor.java
+++ src/main/java/efs/task/functional/PeopleProcessor.java
@@ -1,14 +1,6 @@
 package efs.task.functional;
 
 import java.util.*;
-
-class PeopleProcessor {
-
-    // TODO: kontynuuj implementację metody tak, aby dla kolekcji (people) obiektów klasy Person zwracała listę
-    //       imion ludzi, których wiek jest większy niż podany w argumencie age
-   // List<String> namesOfPeopleWhoseAgeIsGreaterThan(int age, Collection<Person> people) {
-     //   return people.stream()
-    import java.util.*;
 import java.util.stream.Collectors;
 
 class PeopleProcessor {
@@ -32,6 +24,37 @@ class PeopleProcessor {
                 .collect(Collectors.groupingBy(Person::getCityOfLiving, Collectors.averagingInt(Person::getAge)));
     }
 }
+
+//class PeopleProcessor {
+
+    // TODO: kontynuuj implementację metody tak, aby dla kolekcji (people) obiektów klasy Person zwracała listę
+    //       imion ludzi, których wiek jest większy niż podany w argumencie age
+   // List<String> namesOfPeopleWhoseAgeIsGreaterThan(int age, Collection<Person> people) {
+     //   return people.stream()
+  //  import java.util.*;
+//import java.util.stream.Collectors;
+
+//class PeopleProcessor {
+  //  List<String> namesOfPeopleWhoseAgeIsGreaterThan(int age, Collection<Person> people) {
+    //    return people.stream()
+        //        .filter(person -> person.getAge() > age)
+         //       .map(Person::getName)
+          //      .collect(Collectors.toList());
+//   }
+
+   // List<String> namesOfPeopleSortedByAgeLivingIn(String cityOfLiving, Collection<Person> people) {
+        //return people.stream()
+               // .filter(person -> person.getCityOfLiving().equals(cityOfLiving))
+               // .sorted(Comparator.comparingInt(Person::getAge))
+               // .map(Person::getName)
+               // .collect(Collectors.toList());
+    //}
+
+   // Map<String, Double> averageAgeByCityOfLiving(Collection<Person> people) {
+    //    return people.stream()
+    //            .collect(Collectors.groupingBy(Person::getCityOfLiving, Collectors.averagingInt(Person::getAge)));
+    //}
+//}
 
 
     // TODO: kontynuuj implementację metody tak, aby dla kolekcji (people) obiektów klasy Person zwracała listę
